c22f763c44f19586308f04dd468aca9f1a66e3f3
Takuya Iida <wakatakerutmt@ezweb.ne.jp>
2018-11-20 00:17:39 +0900

    現在位置情報取得

--- app/src/main/java/com/example/iidatakuya/mymap/MainActivity.kt
+++ app/src/main/java/com/example/iidatakuya/mymap/MainActivity.kt
@@ -1,17 +1,29 @@
 package com.example.iidatakuya.mymap
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback ,GoogleMap.OnMapLongClickListener{
+
+
+
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
 
     private lateinit var mMap: GoogleMap
+    // 初期位置を六甲山に
+    private val mLatitude = 34.0 + 46.0 / 60 + 41.0 / (60 * 60)
+    private val mLongitude = 135.0 + 15.0 / 60 + 49.0 / (60 * 60)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +32,14 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback ,GoogleMap.OnMapLon
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        // 位置情報パーミッションの通知
+        if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    1000)
+        }
     }
 
     /**
@@ -34,12 +54,26 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback ,GoogleMap.OnMapLon
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-//        test
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        // 長押し処理リスナー
         mMap.setOnMapLongClickListener(this)
+
+
+        val location = LatLng(mLatitude, mLongitude)
+        val cameraPos = CameraPosition.Builder()
+                .target(location).zoom(13.0f)
+                .build()
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPos))
+
+        // MyLocationレイヤーを有効に
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // 位置情報許可しなかった場合はMyLocation表示までいかずreturn
+            return
+        }
+        mMap.isMyLocationEnabled = true
+
+        // MyLocationButtonを有効に
+        val settings = mMap.uiSettings
+        settings.isMyLocationButtonEnabled = true
     }
 
     //  長押し検知
@@ -47,4 +81,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback ,GoogleMap.OnMapLon
         // ピンを立てる
         mMap.addMarker(MarkerOptions().position(p0!!).title("LongClick").draggable(false))
     }
+
+
 }
