f921d257284107d2fc28a909ed22f3fd3a284a2a
skyloken <risora1234@gmail.com>
2018-11-25 22:10:36 +0900

    Realmを導入

---
+++ app/src/main/java/com/example/iidatakuya/mymap/MyMapApplication.kt
@@ -0,0 +1,12 @@
+package com.example.iidatakuya.mymap
+
+import android.app.Application
+import io.realm.Realm
+
+class MyMapApplication : Application(){
+    override fun onCreate() {
+        super.onCreate()
+
+        Realm.init(this)
+    }
+}
