b943579f0dec04dc3b720c27bf84fd1d5de8d66f
skyloken <risora1234@gmail.com>
2018-11-25 22:45:14 +0900

    モデルを追加

--- app/src/main/java/com/example/iidatakuya/mymap/model/Location.kt
+++ app/src/main/java/com/example/iidatakuya/mymap/model/Location.kt
@@ -1,12 +1,16 @@
 package com.example.iidatakuya.mymap.model
 
 import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+import java.util.*
 
 
 class Location : RealmObject() {
-    private val id: Int = 0
+    @PrimaryKey
+    private val id: String = UUID.randomUUID().toString()
     private val name: String? = null
     private val latitude: Double = 0.0
     private val longitude: Double = 0.0
     private val discription: String? = null
+    private val createdAt: Date? = null
 }
