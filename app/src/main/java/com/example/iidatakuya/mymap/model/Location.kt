8cbfa3909b62fce7a34b826f0a33614b7c439a55
skyloken <risora1234@gmail.com>
2018-11-25 22:37:35 +0900

    モデルを追加

---
+++ app/src/main/java/com/example/iidatakuya/mymap/model/Location.kt
@@ -0,0 +1,12 @@
+package com.example.iidatakuya.mymap.model
+
+import io.realm.RealmObject
+
+
+class Dog : RealmObject() {
+    private val id: Int = 0
+    private val name: String? = null
+    private val latitude: Double = 0.0
+    private val longitude: Double = 0.0
+    private val discription: String? = null
+}
